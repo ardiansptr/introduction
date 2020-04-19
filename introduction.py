f44e772f6ab3b63db25627ae7f59194d666d7c45
ardiansptr <35023188+ardiansptr@users.noreply.github.com>
2020-04-19 19:35:11 +0700

    Create introduction.py

---
+++ introduction.py
@@ -0,0 +1,5 @@
+def introduction():
+  name = input('Name: ')
+  address = input('Adress: ')
+  dob = input('Date of Birth: ')
+  print('My Name is {}, I live in {}, and I was born in {}'.format(name,address,dob))
